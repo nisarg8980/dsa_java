a0aafe004e055f26573fa80c08adb7cb9722ab25
Nisarg Patel <nisarg.patel@nowonline.com>
2025-10-27 14:22:04 +0530

    done area of circle problem

---
+++ conditions_loops/InnerAreaofcircle.java
@@ -0,0 +1,8 @@
+public class InnerAreaofcircle {
+
+    public static void main(String[] args) {
+        double radius = 7.5;
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+    }
+}
